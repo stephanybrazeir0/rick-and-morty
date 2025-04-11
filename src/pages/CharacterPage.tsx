1f26b1a9fb25426e58659b27595a93ef5b9b96cb
stephanybrazeir0 <stephanybrazeiro@gmail.com>
2025-04-11 10:28:35 -0300

    feat: add loading and error to character page

--- src/pages/CharacterPage.tsx
+++ src/pages/CharacterPage.tsx
@@ -3,10 +3,11 @@ import { useParams } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import CardDetailingCharacter from "../components/CardDetailingCharacter";
 import DetailingCharacter from "../queries/DetailingCharacter";
+import Spinner from "../components/Spinner";
 
 const CharacterPage = () => {
   const params = useParams();
-  const { data } = useQuery(DetailingCharacter, {
+  const { data, loading, error } = useQuery(DetailingCharacter, {
     variables: { id: params.id },
   });
 
@@ -14,10 +15,16 @@ const CharacterPage = () => {
     <>
       <Navbar />
       <main className="centralized-container">
-        {data ? (
-          <CardDetailingCharacter data={data.character} />
+        {loading ? (
+          <Spinner />
+        ) : error ? (
+          <div>
+            <p className="text-red-900 font-medium text-center p-2">
+              Ocorreu um erro: {error.message}
+            </p>
+          </div>
         ) : (
-          <h1>Erro!</h1>
+          <CardDetailingCharacter data={data.character} />
         )}
       </main>
     </>
